6f8ea94ddf3273236765dfbc756bc1e1731b944b
srinivas <sbelidhe@gmail.com>
2020-09-20 18:31:40 +0530

    Initial Commit

---
+++ django_auth/django_auth/users/views.py
@@ -0,0 +1,70 @@
+from django.shortcuts import render
+from datetime import datetime
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.signals import user_logged_in
+from django.core.exceptions import ObjectDoesNotExist
+
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.generics import UpdateAPIView
+from rest_framework.views import APIView
+from rest_framework_jwt.utils import jwt_payload_handler
+
+from .serializers import *
+from django.conf import settings
+from .models import *
+import jwt
+
+
+class CreateUserAPIView(APIView):
+    # Allow any user (authenticated or not) to access this url 
+    permission_classes = (AllowAny,)
+
+    def post(self, request):
+        user = request.data
+        serializer = UserSerializer(data=user)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+class UpdateUserAPIView(UpdateAPIView):
+    # Allow only authenticated users to access this url
+    permission_classes = (IsAuthenticated,)
+    serializer_class = UpdateUserSerializer
+    queryset = User.objects.all()
+    lookup_field = "id"
+    # serializer_class = SomeSerializer
+
+@api_view(['POST'])
+@permission_classes([AllowAny, ])
+def authenticate_user(request):
+    try:
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.get(email=email, password=password)
+        if user:
+            try:
+                payload = jwt_payload_handler(user)
+                token = jwt.encode(payload, settings.SECRET_KEY)
+                user_details = {}
+                user_details['name'] = "%s %s" % (
+                    user.first_name, user.last_name)
+                user_details['token'] = token
+                user_logged_in.send(sender=user.__class__,
+                                    request=request, user=user)
+                return Response(user_details, status=status.HTTP_200_OK)
+
+            except Exception as e:
+                raise e
+        else:
+            res = {
+                'error': 'can not authenticate with the given credentials or the account has been deactivated'}
+            return Response(res, status=status.HTTP_403_FORBIDDEN)
+    except KeyError:
+        res = {'error': 'please provide a email and a password'}
+        return Response(res)
